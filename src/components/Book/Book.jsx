7f1c2691669f52445f6a6378c42b08303bfef2c3
killshotxd <hassanansari211@gmail.com>
2022-10-07 16:36:49 +0530

    Demo done

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -6,7 +6,79 @@ const Book = () => {
   return (
     <div>
       <Navbar />
-      <div className={styles.container}></div>
+      <div className={styles.main}>
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://images.jdmagicbox.com/quickquotes/images_main/maruti_suzuki_swift_dzire_vdi_pearl_arctic_white_82690415_32.jpg"
+            alt="IMG"
+          />
+          <p>Swift Dzire</p>
+        </div>
+
+        {/* End */}
+
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220830061918_Innova_Crysta.jpeg"
+            alt="IMG"
+          />
+          <p>Innova Crysta</p>
+        </div>
+        {/* End */}
+
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220830061918_Innova_Crysta.jpeg"
+            alt="IMG"
+          />
+          <p>Innova Crysta</p>
+        </div>
+        {/* End */}
+
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220830061918_Innova_Crysta.jpeg"
+            alt="IMG"
+          />
+          <p>Innova Crysta</p>
+        </div>
+        {/* End */}
+
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220830061918_Innova_Crysta.jpeg"
+            alt="IMG"
+          />
+          <p>Innova Crysta</p>
+        </div>
+        {/* End */}
+
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220830061918_Innova_Crysta.jpeg"
+            alt="IMG"
+          />
+          <p>Innova Crysta</p>
+        </div>
+        {/* End */}
+
+        {/* Start */}
+        <div className={styles.container}>
+          <img
+            src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20220830061918_Innova_Crysta.jpeg"
+            alt="IMG"
+          />
+          <p>Innova Crysta</p>
+        </div>
+        {/* End */}
+      </div>
+
       <Footer />
     </div>
   );
