481d576a5148da91846e8a60f2bcc97188b12922
killshotxd <hassanansari211@gmail.com>
2022-10-10 20:43:02 +0530

    responsiveness ++

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -19,8 +19,8 @@ const Services = () => {
         <CCard style={{ width: "22rem" }}>
           <CCardImage orientation="top" src={nepal} />
           <CCardBody>
-            <CCardTitle style={{ color: "black" }}>Nepal</CCardTitle>
-            <CCardText style={{ color: "black" }}>
+            <CCardTitle>Nepal</CCardTitle>
+            <CCardText>
               nepal Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Nemo blanditiis nesciunt provident magni ut! Cumque eos, aut
               impedit perspiciatis dicta vero ratione?
@@ -34,8 +34,8 @@ const Services = () => {
         <CCard style={{ width: "22rem" }}>
           <CCardImage orientation="top" src={lko} />
           <CCardBody>
-            <CCardTitle style={{ color: "black" }}>Lucknow</CCardTitle>
-            <CCardText style={{ color: "black" }}>
+            <CCardTitle>Lucknow</CCardTitle>
+            <CCardText>
               nepal Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Nemo blanditiis nesciunt provident magni ut! Cumque eos, aut
               impedit perspiciatis dicta vero ratione?
@@ -49,8 +49,8 @@ const Services = () => {
         <CCard style={{ width: "22rem" }}>
           <CCardImage orientation="top" src={varanasi} />
           <CCardBody>
-            <CCardTitle style={{ color: "black" }}>Varanasi</CCardTitle>
-            <CCardText style={{ color: "black" }}>
+            <CCardTitle>Varanasi</CCardTitle>
+            <CCardText>
               nepal Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Nemo blanditiis nesciunt provident magni ut! Cumque eos, aut
               impedit perspiciatis dicta vero ratione?
