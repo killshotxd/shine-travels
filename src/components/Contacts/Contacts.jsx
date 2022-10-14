daed58d271533afbc26031c3898607f7b00e1616
killshotxd <hassanansari211@gmail.com>
2022-10-14 18:18:03 +0530

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import styles from "./Contacts.module.css";
 const Contacts = () => {
   return (
-    <div className={styles.container}>
-      <p>
-        Call Us at :{" "}
-        <strong>
-          <a href="tel:+917071707194">7071707194</a>
-        </strong>
-      </p>
-      <p>
-        Call Us at :{" "}
-        <strong>
-          <a href="tel:+919115061597">9115061597</a>
-        </strong>
-      </p>
-    </div>
+    <>
+      {/* <div className={styles.container}>
+        <p>
+          Call Us at :{" "}
+          <strong>
+            <a href="tel:+917071707194">7071707194</a>
+          </strong>
+        </p>
+        <p>
+          Call Us at :{" "}
+          <strong>
+            <a href="tel:+919115061597">9115061597</a>
+          </strong>
+        </p>
+      </div> */}
+    </>
   );
 };
 
