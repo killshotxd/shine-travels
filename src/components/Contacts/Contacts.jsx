3d343f4d7d1cf9dee0784e703fc0f954e011313b
killshotxd <hassanansari211@gmail.com>
2022-10-14 17:20:16 +0530

    Responsiveness

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import styles from "./Contacts.module.css";
 const Contacts = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <p>
+        Call Us at :{" "}
+        <strong>
+          <a href="tel:+917071707194">7071707194</a>
+        </strong>
+      </p>
+      <p>
+        Call Us at :{" "}
+        <strong>
+          <a href="tel:+919115061597">9115061597</a>
+        </strong>
+      </p>
+    </div>
+  );
 };
 
 export default Contacts;
